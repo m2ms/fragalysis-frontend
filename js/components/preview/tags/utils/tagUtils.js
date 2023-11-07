2d8e3c4b8c2ca5804dcca66e4d4a6dea2bc3fe68
Boris Kovar <boris.kovar@m2ms.sk>
2023-11-07 06:42:52 +0100

    - checkpoint

--- js/components/preview/tags/utils/tagUtils.js
+++ js/components/preview/tags/utils/tagUtils.js
@@ -130,44 +130,6 @@ export const getAllTagsForMol = (mol, tagList) => {
   return result;
 };
 
-// export const getCategoryIds = () => {
-//   const result = [];
-
-//   let categoryObject = {
-//     id: CATEGORY_ID[CATEGORY_TYPE.SITE],
-//     category: CATEGORY_TYPE['SITE'],
-//     colour: '00CC00',
-//     description: null
-//   };
-//   result.push({ ...categoryObject });
-
-//   categoryObject = {
-//     id: CATEGORY_ID[CATEGORY_TYPE.SERIES],
-//     category: CATEGORY_TYPE['SERIES'],
-//     colour: '00CC00',
-//     description: null
-//   };
-//   result.push({ ...categoryObject });
-
-//   categoryObject = {
-//     id: CATEGORY_ID[CATEGORY_TYPE.FORUM],
-//     category: CATEGORY_TYPE['FORUM'],
-//     colour: '00CC00',
-//     description: null
-//   };
-//   result.push({ ...categoryObject });
-
-//   categoryObject = {
-//     id: CATEGORY_ID[CATEGORY_TYPE.OTHER],
-//     category: CATEGORY_TYPE['OTHER'],
-//     colour: '00CC00',
-//     description: null
-//   };
-//   result.push({ ...categoryObject });
-
-//   return result;
-// };
-
 export const getDefaultTagDiscoursePostText = tag => {
   return `This post for tag ${tag.tag} is here to discuss its contents.`;
 };

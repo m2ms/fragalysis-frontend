0e5ae6b06cc64324e20beab00d4b0b49d384fba4
boriskovar-m2ms <boris.kovar@m2ms.sk>
2025-02-18 13:56:38 +0100

    Squashed commit of the following:
    commit a3362a9605ca24cb4fc0e15f0b9b40c280979761
    Author: boriskovar-m2ms <boris.kovar@m2ms.sk>
    Date:   Tue Feb 18 12:10:14 2025 +0100
    
        - fixed in code changes of orientation
    
    commit fa136ff8950854e5502d1f557954fa354775638a
    Author: boriskovar-m2ms <boris.kovar@m2ms.sk>
    Date:   Tue Feb 18 08:44:24 2025 +0100
    
        - general implementation is done
    
    commit aa7ee8e8a9eccd248b75f6bd6d8fae093554cb98
    Merge: 121b85dc 43960150
    Author: boriskovar-m2ms <boris.kovar@m2ms.sk>
    Date:   Wed Feb 5 14:40:06 2025 +0100
    
        Merge branch '#1623-WrongOrientation' of https://github.com/m2ms/fragalysis-frontend into #1623-WrongOrientation
    
    commit 121b85dc3441ac0fc6fc150961aebd3aa6b5a387
    Author: boriskovar-m2ms <boris.kovar@m2ms.sk>
    Date:   Wed Feb 5 14:35:49 2025 +0100
    
        - checkpoint
    
    commit 2ba4b17e64f382c7d16f1f804a026fa51f34263c
    Author: boriskovar-m2ms <boris.kovar@m2ms.sk>
    Date:   Wed Feb 5 12:42:27 2025 +0100
    
        - fixing the centering of the ligands
    
    commit 2c9d6af2a9a0432f6d7ce0a301fced84b5a4fae7
    Author: boriskovar-m2ms <boris.kovar@m2ms.sk>
    Date:   Mon Jan 27 11:55:43 2025 +0100
    
        -checkpoint
    
    commit 43960150b09deb90ba53a0423ec232c874ad59af
    Author: boriskovar-m2ms <boris.kovar@m2ms.sk>
    Date:   Wed Feb 5 14:35:49 2025 +0100
    
        - checkpoint
    
    commit 36a569bbba3f8a75055ae45f937ce7eff7b50f93
    Author: boriskovar-m2ms <boris.kovar@m2ms.sk>
    Date:   Wed Feb 5 12:42:27 2025 +0100
    
        - fixing the centering of the ligands
    
    commit 8a6a5115bb98789bbcf2d024eeaeff85243b87d7
    Author: boriskovar-m2ms <boris.kovar@m2ms.sk>
    Date:   Mon Jan 27 11:55:43 2025 +0100
    
        -checkpoint

--- js/components/datasets/datasetMoleculeView/datasetMoleculeView.js
+++ js/components/datasets/datasetMoleculeView/datasetMoleculeView.js
@@ -1119,7 +1119,7 @@ const DatasetMoleculeView = memo(
                         variant="outlined"
                         className={classes.myLocationButton}
                         onClick={() => {
-                          dispatch(centerOnLigandByMoleculeID(stage, getDatasetMoleculeID(datasetID, currentID)));
+                          dispatch(centerOnLigandByMoleculeID(stage, currentID, datasetID));
                         }}
                         disabled={false || !isLigandOn || isCompoundFromVectorSelector(data)}
                       >
@@ -1397,10 +1397,10 @@ const DatasetMoleculeView = memo(
                                   null}
                               </Grid>
                             )) || (
-                                <Grid item className={classes.rightBorder}>
-                                  -
-                                </Grid>
-                              )}
+                              <Grid item className={classes.rightBorder}>
+                                -
+                              </Grid>
+                            )}
                           </Tooltip>
                         );
                       })}

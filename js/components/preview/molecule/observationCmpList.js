80585e41558e2e758d4bc112f227d0be65a08bf0
boriskovar-m2ms <boris.kovar@m2ms.sk>
2025-04-01 09:13:56 +0200

    - #1707 - clean up is done. If additional unnecessary code is found, during implementation of new snapshot navigation, it will be removed

--- js/components/preview/molecule/observationCmpList.js
+++ js/components/preview/molecule/observationCmpList.js
@@ -269,7 +269,7 @@ const useStyles = makeStyles(theme => ({
 }));
 let selectedDisplayHits = false;
 
-export const ObservationCmpList = memo(({ hideProjects }) => {
+export const ObservationCmpList = memo(({}) => {
   const classes = useStyles();
   const dispatch = useDispatch();
   let match = useRouteMatch();
@@ -667,7 +667,6 @@ export const ObservationCmpList = memo(({ hideProjects }) => {
       if (
         majorViewStage &&
         all_mol_lists &&
-        hideProjects &&
         target !== undefined &&
         !areLSHCompoundsInitialized &&
         tags &&
@@ -690,7 +689,6 @@ export const ObservationCmpList = memo(({ hideProjects }) => {
     lhsCompoundsList,
     majorViewStage,
     dispatch,
-    hideProjects,
     target,
     proteinsHasLoaded,
     joinedMoleculeLists,
--- js/components/projects/redux/dispatchActions.js
+++ js/components/projects/redux/dispatchActions.js
@@ -1,93 +1,18 @@
 import {
-  setListOfProjects,
   setCurrentSnapshot,
   resetCurrentSnapshot,
-  setCurrentProjectProperty,
-  setProjectModalIsLoading,
-  setCurrentSnapshotTree,
   setCurrentSnapshotList,
-  setIsLoadingTree,
   setIsLoadingCurrentSnapshot,
-  setCurrentProject,
-  setForceCreateProject,
-  setForceProjectCreated,
-  setIsLoadingListOfProjects,
-  setCurrentProjectDiscourseLink
+  setCurrentProject
 } from './actions';
 import { api, METHOD } from '../../../utils/api';
-import { base_url, URLS } from '../../routes/constants';
-import { setDialogCurrentStep } from '../../snapshot/redux/actions';
-import { createInitSnapshotFromCopy, getListOfSnapshots } from '../../snapshot/redux/dispatchActions';
-import { SnapshotType } from './constants';
-import { DJANGO_CONTEXT } from '../../../utils/djangoContext';
+import { base_url } from '../../routes/constants';
 import { createProjectPost } from '../../../utils/discourse';
 import { setIsSnapshot, setOpenDiscourseErrorModal } from '../../../reducers/api/actions';
 
-import moment from 'moment';
 import _ from 'lodash';
 import { setEntireState } from '../../../reducers/actions';
 import { loadTargetList } from '../../target/redux/dispatchActions';
-import { setOrientation } from '../../../reducers/ngl/dispatchActions';
-import { deepClone } from '../../../utils/objectUtils';
-
-export const assignSnapshotToProject = ({ projectID, snapshotID, ...rest }) => (dispatch, getState) => {
-  dispatch(resetCurrentSnapshot());
-  return api({
-    url: `${base_url}/api/snapshots/${snapshotID}/`,
-    data: { session_project: projectID, ...rest },
-    method: METHOD.PATCH
-  })
-    .then(response =>
-      dispatch(
-        setCurrentSnapshot({
-          id: response.data.id,
-          type: response.data.type,
-          title: response.data.title,
-          author: response.data.author,
-          description: response.data.description,
-          created: response.data.created,
-          children: response.data.children,
-          parent: response.data.parent,
-          data: '[]'
-        })
-      )
-    )
-
-    .catch(error => {
-      throw new Error(error);
-    })
-    .finally(() => {
-      dispatch(getListOfSnapshots());
-    });
-};
-
-export const loadListOfAllProjects = () => (dispatch, getState) => {
-  const userID = DJANGO_CONTEXT['pk'] || null;
-  if (userID !== null) {
-    dispatch(setIsLoadingListOfProjects(true));
-    return api({ url: `${base_url}/api/session-projects/?author=${userID}` })
-      .then(response => dispatch(setListOfProjects((response && response.data && response.data.results) || [])))
-      .finally(() => {
-        dispatch(setIsLoadingListOfProjects(false));
-      });
-  } else {
-    return Promise.resolve();
-  }
-};
-
-export const searchInProjects = title => (dispatch, getState) => {
-  const userID = DJANGO_CONTEXT['pk'] || null;
-  if (userID !== null) {
-    dispatch(setIsLoadingListOfProjects(true));
-    return api({ url: `${base_url}/api/session-projects/?author=${userID}&title=${title}` })
-      .then(response => dispatch(setListOfProjects((response && response.data && response.data.results) || [])))
-      .finally(() => {
-        dispatch(setIsLoadingListOfProjects(false));
-      });
-  } else {
-    return Promise.resolve();
-  }
-};
 
 export const removeSnapshotByID = snapshotID => dispatch => {
   return api({ url: `${base_url}/api/snapshots/${snapshotID}` }).then(response => {
@@ -105,35 +30,6 @@ const removeChildren = (children = []) => dispatch => {
   if (children && children.length > 0) {
     return Promise.all(children.map(childID => dispatch(removeSnapshotByID(childID))));
   }
-};
-
-export const removeSnapshotTree = projectID => dispatch => {
-  return api({ url: `${base_url}/api/snapshots/?session_project=${projectID}&type=INIT` }).then(response => {
-    if (response.data.count === 0) {
-      return Promise.resolve('Not found INITIAL snapshot');
-    } else if (response.data.count === 1) {
-      const tree = parseSnapshotAttributes(response.data.results[0]);
-      if (tree.children && tree.children.length === 0) {
-        return dispatch(removeChildren([tree.id]));
-      }
-      return dispatch(removeChildren(tree.children));
-    }
-  });
-};
-
-export const removeProject = projectID => dispatch => {
-  dispatch(setIsLoadingTree(true));
-  return dispatch(removeSnapshotTree(projectID))
-    .then(() => dispatch(removeSnapshotTree(projectID)))
-    .then(() =>
-      api({ url: `${base_url}/api/session-projects/${projectID}/`, method: METHOD.DELETE }).then(() =>
-        dispatch(loadListOfAllProjects())
-      )
-    )
-    .finally(() => {
-      dispatch(setIsLoadingTree(false));
-      dispatch(getListOfSnapshots());
-    });
 };
 
 export const loadSnapshotByProjectID = projectID => async (dispatch, getState) => {
@@ -240,13 +136,6 @@ export const loadCurrentSnapshotByID = snapshotID => (dispatch, getState) => {
   return Promise.resolve(false);
 };
 
-const loadTargetListPostStateRestore = () => (dispatch, getState) => {
-  let onCancel = () => {};
-  dispatch(loadTargetList(onCancel)).catch(error => {
-    throw new Error(error);
-  });
-};
-
 const parseSnapshotAttributes = data => ({
   id: data.id,
   type: data.type,
@@ -289,52 +178,14 @@ const populateChildren = (children = []) => (dispatch, getState) => {
   }
 };
 
-export const loadSnapshotTree = projectID => (dispatch, getState) => {
-  dispatch(setIsLoadingTree(true));
-  dispatch(setCurrentSnapshotTree(null));
-  return api({ url: `${base_url}/api/snapshots/?session_project=${projectID}&type=INIT` })
-    .then(response => {
-      if (response.data.count > 0) {
-        const tree = parseSnapshotAttributes(response.data.results[0]);
-        dispatch(setCurrentSnapshotTree(tree));
-        return dispatch(populateChildren([tree.id]));
-      }
-    })
-    .finally(() => {
-      dispatch(setIsLoadingTree(false));
-      // dispatch(setIsSnapshotDirty(false));
-    });
-};
-
-export const createProjectFromSnapshotDialog = data => dispatch => {
-  dispatch(setProjectModalIsLoading(true));
-  return api({ url: `${base_url}/api/session-projects/`, method: METHOD.POST, data })
-    .then(response => {
-      const projectID = response.data.id;
-      data.projectID = projectID;
-      dispatch(setCurrentProjectProperty('projectID', projectID));
-      dispatch(setCurrentProjectProperty('title', response.data.title));
-    })
-    .finally(() => {
-      dispatch(setForceCreateProject(false));
-      dispatch(setForceProjectCreated(true));
-      dispatch(setDialogCurrentStep(1));
-    });
-};
-
 export const createProjectDiscoursePost = (projectName, targetName, msg, tags) => (dispatch, getState) => {
-  return createProjectPost(projectName, targetName, msg, tags)
-    .then(response => {
-      dispatch(setCurrentProjectDiscourseLink(response.data['Post url']));
-    })
-    .catch(err => {
-      console.log(err);
-      dispatch(setOpenDiscourseErrorModal(true));
-    });
+  return createProjectPost(projectName, targetName, msg, tags).catch(err => {
+    console.log(err);
+    dispatch(setOpenDiscourseErrorModal(true));
+  });
 };
 
 export const createProject = ({ title, description, target, author, tags, project }) => dispatch => {
-  dispatch(setProjectModalIsLoading(true));
   return api({
     url: `${base_url}/api/session-projects/`,
     method: METHOD.POST,
@@ -348,80 +199,6 @@ export const createProject = ({ title, description, target, author, tags, projec
     const tags = response.data.tags;
 
     return dispatch(setCurrentProject({ projectID, authorID, title, description, targetID, tags }));
-  });
-};
-
-const copySnapshot = (selectedSnapshot, projectID, history) => dispatch => {
-  return dispatch(
-    createInitSnapshotFromCopy({
-      title: selectedSnapshot.title,
-      author: (selectedSnapshot && selectedSnapshot.author && selectedSnapshot.author.id) || null,
-      description: selectedSnapshot.description,
-      data: '[]',
-      created: selectedSnapshot.created,
-      parent: null,
-      children: selectedSnapshot.children,
-      session_project: projectID
-    })
-  )
-    .then(() => {
-      history.push(`${URLS.projects}${projectID}`);
-    })
-    .finally(() => {
-      dispatch(setProjectModalIsLoading(false));
-    });
-};
-
-export const createProjectFromSnapshot = ({ title, description, author, tags, history, parentSnapshotId }) => (
-  dispatch,
-  getState
-) => {
-  const listOfSnapshots = getState().snapshotReducers.listOfSnapshots;
-  const currentProject = getState().targetReducers.currentProject;
-  const selectedSnapshot = listOfSnapshots.find(item => item.id === parentSnapshotId);
-  const snapshotData = JSON.parse(selectedSnapshot && selectedSnapshot.data);
-
-  dispatch(setProjectModalIsLoading(true));
-  // dispatch(resetTrackingState());
-  // dispatch(resetNglTrackingState());
-  return dispatch(
-    createProject({
-      title,
-      description,
-      target: (snapshotData && snapshotData.apiReducers && snapshotData.apiReducers.target_on) || null,
-      author,
-      tags,
-      project: currentProject?.id
-    })
-  ).then(() => {
-    const { projectID } = getState().projectReducers.currentProject;
-
-    // in case when snapshot has assigned project => make copy
-    if (selectedSnapshot && selectedSnapshot.session_project !== null) {
-      return dispatch(copySnapshot(selectedSnapshot, projectID, history));
-    }
-    // in case when snapshot has not assigned project => mark snapshot as INIT and assign to project
-    else if (selectedSnapshot && selectedSnapshot.session_project === null) {
-      // in case when snapshot has no parent, use given snapshot, but mark it as INIT
-      if (!selectedSnapshot.parent) {
-        return dispatch(
-          assignSnapshotToProject({
-            projectID,
-            snapshotID: selectedSnapshot.id,
-            type: SnapshotType.INIT
-          })
-        )
-          .then(() => {
-            history.push(`${URLS.projects}${projectID}`);
-          })
-          .finally(() => {
-            dispatch(setProjectModalIsLoading(false));
-          });
-      } // in case when snapshot has parent => create new snapshot with INIT type and copy all data from previous snapshot
-      else {
-        return dispatch(copySnapshot(selectedSnapshot, projectID, history));
-      }
-    }
   });
 };
 

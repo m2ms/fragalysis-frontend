4b9cd79054b01f52c92fc8ff9a7acfed8f7c38ae
Boris Kovar <boris.kovar@m2ms.sk>
2025-04-22 13:57:09 +0200

    - adds new rendering library for screenshots

--- js/components/userFeedback/browserApi.js
+++ js/components/userFeedback/browserApi.js
@@ -1,5 +1,5 @@
 import { setImageSource, setIsOpenForm } from './redux/actions';
-import html2canvas from 'html2canvas';
+import html2canvas from 'html2canvas-pro';
 /* Getting image from screen capture or  */
 
 // https://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser
@@ -83,7 +83,7 @@ export const captureScreen = () => async dispatch => {
 };
 
 export const captureScreenOfSnapshot = () => async dispatch => {
-  // html2canvas(document.body).then(canvas => {
-  //   // dispatch(setTrackingImageSource(canvas.toDataURL()));
-  // });
+  return html2canvas(document.body).then(canvas => {
+    return canvas.toDataURL();
+  });
 };

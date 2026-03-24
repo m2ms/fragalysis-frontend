7a84e164800811ff20654f62c02a15338cd386a7
Samuel Pekar <Ikjn3op8!@#>
2026-03-24 14:53:42 +0100

    #1977 add action for reducer for artefacts chain

--- js/reducers/selection/actions.js
+++ js/reducers/selection/actions.js
@@ -104,6 +104,31 @@ export const removeFromProteinList = function(item, skipTracking = false) {
     skipTracking: skipTracking
   };
 };
+
+export const setArtefactChainList = function(artefactChainList, skipTracking = false) {
+  return {
+    type: constants.SET_ARTEFACTS_CHAIN_LIST,
+    artefactChainList: artefactChainList,
+    skipTracking
+  };
+};
+
+export const appendArtefactsChainList = function(item, skipTracking = false) {
+  return {
+    type: constants.APPEND_ARTEFACTS_CHAIN_LIST,
+    item: item,
+    skipTracking: skipTracking
+  };
+};
+
+export const removeFromArtefactsChainList = function(item, skipTracking = false) {
+  return {
+    type: constants.REMOVE_FROM_ARTEFACTS_CHAIN_LIST,
+    item: item,
+    skipTracking: skipTracking
+  };
+};
+
 export const setComplexList = function(complexList, skipTracking = false) {
   return {
     type: constants.SET_COMPLEX_LIST,

1a71cff77647d03c6593b25b245c2245bd1ae02e
Samuel Pekar <Ikjn3op8!@#>
2026-06-25 17:57:45 +0200

    #2127 remove log

--- js/components/target/targetSettingsModal.js
+++ js/components/target/targetSettingsModal.js
@@ -170,7 +170,6 @@ export const TargetSettingsModal = memo(({ openModal, onModalClose, isTargetOn =
   };
 
   const onSubmitForm = async () => {
-    console.log('submit form', currentTarget);
     currentTarget &&
       api({
         url: `${base_url}/api/targets/${currentTarget.id}/`,
